2e95f38aced9d79b01012956e340384baf06a602
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2022-08-06 18:56:35 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int x =0;
+		cin >> x;
+		sum += x;
+	}
+	cout << sum;
+	
+	return 0;
+}
