5ed9cba54105dbc99100ce7ad9db540a11ca7358
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2022-08-31 16:56:28 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 12 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.cc
@@ -1,17 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include<string>
+#include<cstring>
+#include<vector>
+
 using namespace std;
 
-const int mod=1e9+7;
-long long dp[1010][1010]={0};
+const int MAX = 1000 + 1;
+const int mod = 1000000007;
+int n, m;
+long long dp[MAX][MAX] = { 0, };
 
-int main(){
-	dp[1][1]=1;
-	int n, m;
-	scanf("%d %d", &n, &m);
-	for(int i=1; i<=n; i++){
-		for(int j=1; j<=m; j++){
-			if(i*j!=1) dp[i][j] = (dp[i][j-1]%mod + dp[i-1][j]%mod + dp[i-1][j-1]%mod)%mod;
-		}
-	}
-	printf("%lld", dp[n][m]%mod);
+int main() 
+{
+    cin >> n >> m;
+    dp[1][1] = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if(i * j != 1)
+                dp[i][j] = (dp[i - 1][j]%mod + dp[i - 1][j - 1] % mod + dp[i][j - 1] % mod) % mod;
+        }
+    }
+    cout << dp[n][m] % mod << endl;
+    return 0;
+
 }
