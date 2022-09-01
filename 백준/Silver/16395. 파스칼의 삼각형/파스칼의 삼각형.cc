9553df2b73acfc5f0841f8c7d0c94243e8c71208
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2022-09-01 16:06:01 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+
+int dp[31][31];
+
+int main(void) {
+	cin >> n >> k;
+	for (int i = 0; i < 31; i++)
+	{
+		dp[i][0] = 1;
+	}
+	for (int i = 1; i < 31; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+	}
+	cout << dp[n - 1][k - 1] << endl;
+	return 0;
+}
