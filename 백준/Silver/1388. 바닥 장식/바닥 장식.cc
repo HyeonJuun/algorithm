35fea4f90c51e219723190052b669675bdbb33e3
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2022-10-29 00:03:11 +0900

    [Silver III] Title: 바닥 장식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.cc
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.cc
@@ -1,13 +1,92 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+
 using namespace std;
-int n, m, i, t, c; string s[105];
-int main() {
-	cin >> n >> m;
-	for (; i < n; i++)	cin >> s[i];
-	for (i = 0; i < n; i++)
-		for (t = 0; t < m; t++) {
-			if (s[i][t] == '-')if (t == m - 1 || s[i][t + 1] == '|')c++;
-			if (s[i][t] == '|')if (i == n - 1 || s[i + 1][t] == '-')c++;
+
+const int MAX = 51;
+
+char map[MAX][MAX];
+bool visited[MAX][MAX];
+
+int n, m;
+
+int dx[] = { 1, -1};
+int dy[] = {-1 ,1 };
+
+void bfs(int y, int x)
+{
+	queue<pair<int, int>> q;
+	q.push({ y, x });
+	visited[y][x] = true;
+
+	while (!q.empty())
+	{
+		int xx = q.front().second;
+		int yy = q.front().first;
+		q.pop();
+		if (map[yy][xx] == '-')
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				int nx = xx + dx[i];
+				if (nx < 0 || nx > m)
+					continue;
+				if (!visited[yy][nx] && map[yy][nx] == '-')
+				{
+					q.push({ yy, nx });
+					visited[yy][nx] = true;
+				}
+			}
 		}
-	cout << c << endl;
+		else if (map[yy][xx] == '|')
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				int ny = yy + dy[i];
+				if (ny < 0 || ny>n)
+					continue;
+				if (!visited[ny][xx] && map[ny][xx] == '|')
+				{
+					q.push({ ny, xx });
+					visited[ny][xx] = true;
+
+				}
+			}
+		}
+		
+	}
+}
+
+int ans = 0;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cin >> map[i][j];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (!visited[i][j])
+			{
+				bfs(i, j);
+				ans++;
+			}
+		}
+	}
+	cout << ans << '\n';
+	return 0;
 }
