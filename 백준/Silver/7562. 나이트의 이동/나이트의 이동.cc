d1784d94752a6bcbd1764d52caaacf2ebd88f4a1
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2022-12-28 16:44:55 +0900

    [Silver I] Title: 나이트의 이동, Time: 28 ms, Memory: 2596 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
-#include<cstring>
+#include <cmath>
+#include<string>
+#include <cstring>
+#include<queue>
+#include<deque>
 
 using namespace std;
 
 const int MAX = 300 + 1;
 
-int t, n, cnt = 0;
-int start_x, start_y, end_x, end_y;
+int t, n;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
 
-int dx[8] = { -2, -2, -1, -1, 1, 1, 2, 2 };
-int dy[8] = { 1, -1, 2, -2, 2, -2, 1, -1 };
+int dx[] = { -2, -2, -1, -1, 1, 1, 2, 2 };
+int dy[] = { 1, -1, 2, -2, 2, -2, 1, -1 };
+int ans;
 
-int map[MAX][MAX] = { 0, };
-bool visited[MAX][MAX] = { false, };
 
-void bfs(int sx, int sy)
+void bfs(int start_x, int start_y, int end_x, int end_y)
 {
-	queue < pair<int, int> > q;
-
-	visited[sx][sy] = true;
-	q.push(make_pair(sx, sy));
+	queue<pair<int, int>> q;
+	q.push({ start_x, start_y });
+	visited[start_x][start_y] = true;
 
 	while (!q.empty())
 	{
@@ -38,30 +40,34 @@ void bfs(int sx, int sy)
 		{
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+		
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
 				continue;
+
 			if (!visited[nx][ny])
 			{
-				visited[nx][ny] = 1;
-				q.push(make_pair(nx, ny));
+				visited[nx][ny] = true;
+				q.push({ nx, ny });
 				map[nx][ny] = map[x][y] + 1;
 			}
 		}
 	}
 }
-
-int main(void)
+int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	cin >> t;
+
 	while (t--)
 	{
 		cin >> n;
-		cin >> start_x >> start_y;
-		cin >> end_x >> end_y;
-		bfs(start_x, start_y);
-		memset(map, 0, sizeof(map));
+		int start_x, start_y, end_x, end_y;
+		cin >> start_x >> start_y >> end_x >> end_y;
+		bfs(start_x, start_y, end_x, end_y);
 		memset(visited, false, sizeof(visited));
+		memset(map, 0, sizeof(map));
 	}
 	return 0;
 }
