f5ad129dfbdafd9b1168cc19991ea7687e9063ce
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-01-05 20:15:37 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,28 +1,44 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
-#include<queue>
-#include<cstring>
-#include<string>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
 const int MAX = 10000 + 1;
 
-int dp[MAX] = { 0, };
-int arr[MAX] = { 0, };
-int n, x, ans=0;
+int n;
+
+int arr[MAX];
+int dp[MAX][3] = { 0 };
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	cin >> n;
 	for (int i = 1; i <= n; i++)
+	{
 		cin >> arr[i];
-	dp[0] = 0;
-	dp[1] = arr[1];
-	dp[2] = arr[1] + arr[2];
+	}
+	dp[1][1] = arr[1];
+	dp[2][1] = arr[2];
+	dp[2][2] = arr[1] + arr[2];
 	for (int i = 3; i <= n; i++)
 	{
-		dp[i] = max(dp[i-3] + arr[i-1] + arr[i], max(dp[i-2] + arr[i], dp[i-1]));
+		dp[i][0] = max(dp[i - 2][1], max(dp[i - 1][0], dp[i - 1][2]));
+		dp[i][1] = max(dp[i - 1][0], max(dp[i - 2][1], dp[i - 2][2])) + arr[i];
+		dp[i][2] = dp[i - 1][1] + arr[i];
 	}
-	cout << dp[n] << endl;
+	int ans = -1;
+	for (int i = 0; i < 3; i++)
+	{
+		if (ans < dp[n][i])
+			ans = dp[n][i];
+	}
+	cout << ans;
 	return 0;
 }
