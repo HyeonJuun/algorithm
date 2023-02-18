4104efa779f9b8b95c3849da7ad9031bb3ee4ab7
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-02-18 13:39:48 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 7980 ms, Memory: 5192 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.cc
@@ -1,25 +1,48 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <queue>
 #include <algorithm>
+#include <string>
+#include <cstring>
 #include <map>
-
 using namespace std;
-using  ll = long long;
 
-int main() {
-    int t; scanf("%d", &t);
-    while (t--) {
-        map<ll, int> cnt;
-        ll ret = 0, idx = -1;
-        int n; scanf("%d", &n);
-        for (int i = 0; i < n; i++) {
-            ll num; scanf("%lld", &num);
-            ll tmp = ++cnt[num];
-            if (tmp > ret) {
-                idx = num;
-                ret = tmp;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        map<long long, int> mp;
+        long long mx = -1, idx = -1;
+        int num;
+        cin >> num;
+        for (int i = 0; i < num; i++)
+        {
+            long long x;
+            cin >> x;
+            if (mp[x] == 0)
+            {
+                mp[x] = 1;
+            }
+            else
+            {
+                mp[x]++;
+            }
+            if (mp[x] > mx)
+            {
+                idx = x;
+                mx = mp[x];
             }
         }
-        if (ret > n / 2) printf("%lld\n", idx);
-        else printf("SYJKGW\n");
+        if (mx > num / 2)
+            cout << idx << '\n';
+        else
+            cout << "SYJKGW\n";
     }
+    return 0;
 }
