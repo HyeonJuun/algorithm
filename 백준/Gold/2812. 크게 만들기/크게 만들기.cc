940d7155b3ce8c56f3b07ad2f6a102e1c67a9684
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-02-23 23:46:36 +0900

    [Gold III] Title: 크게 만들기, Time: 12 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int n, k;
+string val;
+deque <char> deq;
+int main() {
+	iostream::sync_with_stdio(0);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	cin >> val;
+
+	for (int i = 0; i < val.length(); i++) {
+
+		while (k && !deq.empty() && deq.back() < val[i]) {
+			deq.pop_back();
+			k--;
+		}
+		deq.push_back(val[i]);
+	}
+
+
+	for (int i = 0; i < deq.size() - k; i++) 
+		cout << deq[i];
+	
+	cout << "\n";
+
+	return 0;
+}
