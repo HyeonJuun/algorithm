299464d46062d105b57c2bacbd9cdc8d1af23eec
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-04-02 20:10:18 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    map<string, int> mp;
+    for(int i=0; i<name.size(); i++)
+    {
+        mp[name[i]] = yearning[i];
+    }
+    
+    for(int i=0; i<photo.size(); i++)
+    {
+        int score = 0;
+        for(int j=0; j<photo[i].size(); j++)
+        {
+            score += mp[photo[i][j]];
+        }
+        answer.push_back(score);
+    }
+    return answer;
+}
