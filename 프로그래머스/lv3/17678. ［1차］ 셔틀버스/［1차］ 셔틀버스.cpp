5f17614e13c8d152900cf0351b96c3d0b8b754ac
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-08-30 18:17:37 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.37 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -5,29 +5,32 @@
 
 using namespace std;
 
+// "hh:mm" -> X분으로 바꾸는 Key 기억하기
+// 첫 버스가 시작되는 09:00 -> 540으로 변경
+// 마지막 버스일 때 m명 안에 드는지 확인하고 못든다면 마지막 사람보다 1분 전에 오기
+// 마지막으로 X분 -> "hh:mm"으로 바꾸는 방법은 substr + string 디테일을 살려야 한다.
 string solution(int n, int t, int m, vector<string> timetable) {
     string answer = "";
-    int res = 0;
+    
     sort(timetable.begin(), timetable.end());
     
     vector<int> tt;
-    for(auto time : timetable)
+    for(int i=0; i<timetable.size(); i++)
     {
-        tt.push_back(stoi(time.substr(0, 2)) * 60 + stoi(time.substr(3, 2)));
+        tt.push_back(stoi(timetable[i].substr(0, 2)) * 60 + stoi(timetable[i].substr(3, 2)));
     }
-    
-    int ind = 0;
+    int index = 0;
     int bus = 540;
-    
-    for(int i = 1; i<= n; i++)
+    int res = 0;
+    for(int i=1; i<=n; i++)
     {
         int num = 0;
-        while(num < m && ind < tt.size())
+        while(num < m && index < tt.size())
         {
-            if(tt[ind] <= bus)
+            if(tt[index] <= bus)
             {
                 num++;
-                ind++;
+                index++;
             }
             else
                 break;
@@ -37,26 +40,21 @@ string solution(int n, int t, int m, vector<string> timetable) {
             if(num < m)
                 res = bus;
             else
-                res = tt[ind-1] - 1;
+                res = tt[index - 1] - 1;
         }
         bus += t;
     }
-    
     int h = res / 60;
-    if(h <= 9)
-    {
+    int mi = res % 60;
+    
+    if(h < 10)
         answer += "0" + to_string(h) + ":";
-    }
     else
         answer += to_string(h) + ":";
     
-    
-    int min = res % 60;
-    if(min <= 9)
-    {
-        answer += "0" + to_string(min);
-    }
+    if(mi < 10)
+        answer += "0" + to_string(mi);
     else
-        answer += to_string(min);
+        answer += to_string(mi);
     return answer;
 }
