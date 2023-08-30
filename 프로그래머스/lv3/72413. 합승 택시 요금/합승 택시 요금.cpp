8f094cc4ecb29bb34a0c2d9601749e60b312a97c
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-08-30 17:33:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 0.97 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,16 +1,15 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <cstring>
 using namespace std;
 
-
-const int INF = 20000000;
+const int INF = 987654321;
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    int answer = INF;
-    vector<vector<int>> dist(n + 1, vector<int>(n+1, INF));
     
+    vector<vector<int>> dist(n + 1, vector<int>(n+1, INF));
+    long long answer = INF;
     for(int i=0; i<fares.size(); i++)
     {
         dist[fares[i][0]][fares[i][1]] = fares[i][2];
@@ -19,19 +18,25 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     for(int i=1; i<=n; i++)
         dist[i][i] = 0;
     
-    for(int k= 1; k<=n; k++)
+    for(int i=1; i<=n; i++)
     {
-        for(int i=1; i<=n; i++)
+        for(int j=1; j<=n; j++)
         {
-            for(int j=1; j<=n; j++)
+            if(i == j)
+                continue;
+            
+            for(int k=1; k<=n; k++)
             {
-                if(dist[i][j] > dist[i][k] + dist[k][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                if(dist[j][i] + dist[i][k] < dist[j][k])
+                    dist[j][k] = dist[j][i] + dist[i][k];
             }
         }
     }
-    for(int i=1; i<= n; i++)
+    
+    for(int i=1; i<=n; i++)
+    {            
         if(answer > dist[s][i] + dist[i][a] + dist[i][b])
-            answer = dist[s][i] + dist[i][a] + dist[i][b];
+            answer = dist[s][i] + dist[i][a] + dist[i][b];        
+    }
     return answer;
 }
