57da66b92393f2974f07538a18f2f8f9b394aa27
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-09-04 20:43:31 +0900

    [unrated] Title: 아방가르드 타일링, Time: 1.40 ms, Memory: 5.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int MOD = 1000000007;
+
+long long dp[100002] = { 1, 1, 3, 10, 23, 62, 170, };
+long long sum[100002] = { 1, 1, 3, 11, 24, 65, 181, };
+int solution(int n) {
+    
+    for (int i = 7; i <= n; i++) {
+		dp[i] = dp[i - 1];
+		dp[i] += dp[i - 2] * 2;
+		dp[i] += dp[i - 3] * 5;
+		dp[i] += sum[i - 4] * 2;
+		dp[i] += sum[i - 5] * 2;
+		dp[i] += sum[i - 6] * 4;
+		dp[i] %= MOD;
+
+		sum[i] = dp[i] + sum[i - 3];
+		sum[i] %= MOD;
+	}
+
+    return dp[n];
+}
