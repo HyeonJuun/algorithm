560f222787493dcfc84b05a44fdf0655ad972780
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-09-06 19:09:40 +0900

    [level 4] Title: [3차] 자동완성, Time: 243.80 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.cpp
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.cpp
@@ -2,39 +2,53 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int solution(vector<string> words) {
-    int answer = 1, pre = 0;
+struct TRIE
+{
+    int count = 0;
+    unordered_map<char, TRIE*> child;
     
-    sort(words.begin(), words.end());
-    
-    string preWord = words[0];
-
-    for(int i=1; i<words.size(); i++)
+    void Insert(string str)
     {
-        int idx = 0;
-        
-        for(; idx<words[i].size(); idx++)
+        TRIE* now = this;
+        for(int i=0; i<str.length(); i++)
         {
-            if(idx >= preWord.size())
+            if(now->child[str[i]] == nullptr)
             {
-                answer += preWord.size() - pre - 1;
-                answer += idx + 1;
-                break;
+                now->child[str[i]] = new TRIE;
             }
-            
-            if(words[i][idx] != preWord[idx])
-            {
-                if(idx > pre)
-                    answer += idx - pre;
-                answer += idx + 1;
-                break;
-            }
+            now = now->child[str[i]];
+            now->count++;
         }
-        pre = idx;
-        preWord = words[i];
-    } 
+    }
+    
+    int AutoComplete(string str)
+    {
+        int input = 1;
+        TRIE* now = child[str[0]];
+        for(int i=1; i<str.length(); i++)
+        {
+            if(now->count > 1)
+                input++;
+            now = now->child[str[i]];
+        }
+        return input;
+    }
+};
+int solution(vector<string> words) {
+    int answer = 0;
+    
+    TRIE root;
+    
+    for(string word : words)
+        root.Insert(word);
+    
+    for(string word : words)
+        answer += root.AutoComplete(word);
+    
+    
     return answer;
 }
