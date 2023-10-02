e78c80454c950bd2b164992fbeea98db6f1eb7eb
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-10-02 18:16:35 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/lv3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -4,84 +4,90 @@
 
 using namespace std;
 
-struct TREE
-{
-    int ind;
+vector<int> pre;
+vector<int> post;
+
+struct TREE{
+    int num;
     int x, y;
-    TREE *leftChild;
-    TREE *rightChild;
+    TREE *leftNode;
+    TREE *rightNode;
 };
-bool cmp(TREE a, TREE b)
+
+bool cmp(TREE A, TREE B)
 {
-    if(a.y >= b.y)
-    {
-        if(a.y == b.y)
-        {
-            if(a.x < b.x)
-                return true;
-            return false;
-        }
-        return true;
-    }
-    return false;
+    if(A.y == B.y)
+        return A.x < B.x;
+    return A.y > B.y;
 }
-void MakeTree(TREE *root, TREE* child)
+void MakeTree(TREE *root, TREE* node)
 {
-    if(root->x > child->x)
+    if(root->x > node->x)
     {
-        if(root->leftChild == NULL)
+        if(root->leftNode == NULL)
         {
-            root->leftChild = child;
+            root->leftNode = node;
             return;
         }
-        MakeTree(root->leftChild, child);
+        else
+        {
+            MakeTree(root->leftNode, node);
+        }
     }
     else
     {
-        if(root->rightChild == NULL)
+        if(root->rightNode == NULL)
         {
-            root->rightChild = child;
+            root->rightNode = node;
             return;
         }
-        MakeTree(root->rightChild, child);
+        else
+        {
+            MakeTree(root->rightNode, node);
+        }
     }
 }
-void PreOrder(TREE* root, vector<int> &answer)
+void PreOrder(TREE* node)
 {
-    if(root == NULL)
+    if(node == NULL)
         return;
-    answer.push_back(root->ind);
-    PreOrder(root->leftChild, answer);
-    PreOrder(root->rightChild, answer);
+    
+    pre.push_back(node->num);
+    PreOrder(node->leftNode);
+    PreOrder(node->rightNode);
 }
-void PostOrder(TREE* root, vector<int> &answer)
+
+void PostOrder(TREE* node)
 {
-    if(root == NULL)
+    if(node == NULL)
         return;
-    PostOrder(root->leftChild, answer);
-    PostOrder(root->rightChild, answer);
-    answer.push_back(root->ind);
+    PostOrder(node->leftNode);
+    PostOrder(node->rightNode);
+    post.push_back(node->num);
 }
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
     vector<vector<int>> answer;
-    vector<TREE> tree;
     
+    vector<TREE> treeV;
     for(int i=0; i<nodeinfo.size(); i++)
     {
         int x = nodeinfo[i][0];
         int y = nodeinfo[i][1];
-        int ind = i + 1;
-        tree.push_back({ind, x, y, NULL, NULL});
+        int num = i + 1;
+        treeV.push_back({num, x, y, NULL, NULL});
     }
-    sort(tree.begin(), tree.end(), cmp);
     
-    TREE *root = &tree[0];
-    for(int i=1; i< tree.size(); i++)
-        MakeTree(root, &tree[i]);
-    vector<int> pre_ans, post_ans;
-    PreOrder(root, pre_ans);
-    PostOrder(root, post_ans);
-    answer.push_back(pre_ans);
-    answer.push_back(post_ans);
+    sort(treeV.begin(), treeV.end(), cmp);
+    
+    TREE *root = &treeV[0];
+    
+    for(int i=1; i<treeV.size(); i++)
+    {
+        MakeTree(root, &treeV[i]);
+    }
+    PreOrder(root);
+    PostOrder(root);
+    answer.push_back(pre);
+    answer.push_back(post);
     return answer;
 }
