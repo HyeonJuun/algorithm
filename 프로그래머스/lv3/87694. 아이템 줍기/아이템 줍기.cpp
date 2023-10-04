31e8a7f9d553996efb7bbb00b14cd95dfdaf107c
hyeon jjun <81156795+HyeonJuun@users.noreply.github.com>
2023-10-04 16:05:49 +0900

    [level 3] Title: 아이템 줍기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -3,51 +3,65 @@
 
 using namespace std;
 
-int visited[101][101];// 1~50까지 칸
-int answer = 987654321;
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
 
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
+int visited[101][101];
+int answer = 2e9;
 
-void dfs(int startx, int starty, int itemx, int itemy, int cnt)
+void dfs(int curX, int curY, int itemX, int itemY, int cnt)
 {
-    if(startx == itemx && starty == itemy)
+    if(curX == itemX && curY == itemY)
     {
-        answer = min(answer, cnt);
+        answer = min(cnt, answer);
         return;
     }
     
     for(int i=0; i<4; i++)
     {
-        int nx = startx + dx[i];
-        int ny = starty + dy[i];
+        int nx = curX + dx[i];
+        int ny = curY + dy[i];
         
-        if(visited[nx][ny] == 7)
+        if(visited[nx][ny] == 2)
         {
             visited[nx][ny] = 0;
-            dfs(nx, ny, itemx, itemy, cnt + 1);
-            visited[nx][ny] = 7;
+            dfs(nx, ny, itemX, itemY, cnt + 1);
+            visited[nx][ny] = 2;
         }
     }
 }
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    characterX *= 2, characterY *= 2, itemX *= 2, itemY *= 2;
     
-    for(int i=0;i<rectangle.size();i++){// 모든사각형 칠하기
-        for(int a=rectangle[i][0]*2;a<=rectangle[i][2]*2;a++){
-            for(int b=rectangle[i][1]*2;b<=rectangle[i][3]*2;b++)
-                visited[a][b]=7;
+    characterX *= 2;
+    characterY *= 2;
+    itemX *= 2;
+    itemY *= 2;
+    
+    for(int i=0; i<rectangle.size(); i++)
+    {
+        for(int x = rectangle[i][0] *2; x <=rectangle[i][2] * 2;x++)
+        {
+            for(int y = rectangle[i][1] * 2; y<= rectangle[i][3] * 2; y++)
+            {
+                visited[x][y] = 2;
+            }
         }
     }
     
-    for(int i=0;i<rectangle.size();i++){// 속 파내기
-        for(int a=rectangle[i][0]*2+1;a<=rectangle[i][2]*2-1;a++){
-            for(int b=rectangle[i][1]*2+1;b<=rectangle[i][3]*2-1;b++)
-                visited[a][b]=0;
+    for(int i=0; i<rectangle.size(); i++)
+    {
+        for(int x = rectangle[i][0] * 2 + 1; x <= rectangle[i][2] * 2 - 1; x++)
+        {
+            for(int y = rectangle[i][1] * 2 + 1; y<= rectangle[i][3] * 2 - 1; y++)
+            {
+                visited[x][y] = 0;
+            }
         }
     }
+    
     visited[characterX][characterY] = 1;
     dfs(characterX, characterY, itemX, itemY, 0);
+        
     
     return answer / 2;
 }
