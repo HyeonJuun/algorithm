3c06f0790401d70a25c6d13e14bb9c5d062dc09d
Hyeon Juun <81156795+HyeonJuun@users.noreply.github.com>
2024-04-22 13:03:53 +0900

    [Platinum V] Title: 영우의 기숙사 청소, Time: 20 ms, Memory: 3520 KB -BaekjoonHub

---
+++ 백준/Platinum/15806. 영우의 기숙사 청소/영우의 기숙사 청소.cc
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+using tpi = tuple<int,int,int>;
+
+int N,M,K,T;
+bool vi[2][301][301]; // 2일을 주기로 증식 확인
+int dy[8] = {-1,-2,-2,-1,1,2,2,1};
+int dx[8] = {-2,-1,1,2,2,1,-1,-2};
+
+void move(int k){
+    queue<tpi> q1, q2;
+    int a,b;
+    for(int i=0; i<M; i++){
+        cin>>a>>b;
+        q1.push({0,a,b});
+    }
+    
+    for(int t=1; t<=k; t++){
+        queue<tpi> &q = t%2 ? q1 : q2;
+        queue<tpi> &nq = t%2 ? q2 : q1;
+        if(q.empty()) break;
+
+        while(!q.empty()){
+            int cm, cy, cx;
+            tie(cm, cy, cx) = q.front();
+            q.pop();
+
+            for(int i=0; i<8; i++){
+                int nm = cm^1; // 2일을 주기로 증식 확인
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+                if(ny<1 || nx<1 || ny>N || nx>N) continue;
+                if(vi[nm][ny][nx]) continue;
+                nq.push({nm, ny, nx});
+                vi[nm][ny][nx] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N>>M>>K>>T;
+
+    move(T);
+    int a,b;
+    for(int i=0; i<K; i++){
+        cin>>a>>b;
+        if(vi[T%2][a][b]) {
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+}
