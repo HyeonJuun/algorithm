62c31dafcc97910a12146e28837f221d820ee275
Hyeon Juun <81156795+HyeonJuun@users.noreply.github.com>
2024-06-10 14:32:17 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -1,71 +1,80 @@
-#include<iostream>
-#include<algorithm>
-#include<queue>
-#include<vector>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define MAX 100 + 1
+
 using namespace std;
 
-const int MAX = 102;
+int n, m, k, answer;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
 
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
 
-int map[MAX][MAX] = { 0, };
-bool visited[MAX][MAX] = { false, };
-
-queue<pair<int, int> > q;
-int area = 1;
-vector<int> ans;
-int h, w, k;
-void bfs(int y, int x)
+int solve(int x, int y)
 {
-	q.push(make_pair(y, x));
-	visited[y][x] = true;
-	while (!q.empty())
-	{
-		y = q.front().first;
-		x = q.front().second;
-		q.pop();
+    int cnt = 1;
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    visited[x][y] = true;
+    // cout << "x : " << x << " y : " << y << endl;
+    while (!q.empty())
+    {
+        int curx = q.front().first;
+        int cury = q.front().second;
+        q.pop();
 
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx<0 || ny<0 || nx>w || ny >h)
-				continue;
-			if (map[ny][nx] == 1 && visited[ny][nx] == 0)
-			{
-				visited[ny][nx] = true;
-				q.push(make_pair(ny, nx));
-				area++;
-			}
-		}		
-		
-	}
-	
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
+
+            if (nx <= 0 || ny <= 0 || nx > n || ny > m || map[nx][ny] == 0)
+                continue;
+
+            if (!visited[nx][ny])
+            {
+                q.push({nx, ny});
+                visited[nx][ny] = true;
+                cnt++;
+            }
+        }
+    }
+    return cnt;
 }
-
 int main()
 {
-	cin >> h >> w >> k;
-	for (int i = 0; i < k; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		map[x][y] = 1;
-	}
-	for (int i = 1; i <= h; i++)
-	{
-		for (int j = 1; j <= w; j++)
-		{
-			if (map[i][j] == 1 && visited[i][j] == 0)
-			{
-				bfs(i, j);
-				ans.push_back(area);
-				area = 1;
-			}
-		}
-	}
-	sort(ans.begin(), ans.end(), greater<>());
-	cout << ans[0] << endl;
-	return 0;
+    cin >> n >> m >> k;
+    for (int i = 0; i < k; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        map[x][y] = 1;
+    }
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= m; j++)
+    //     {
+    //         cout << map[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (!visited[i][j] && map[i][j] == 1)
+            {
+                int x = solve(i, j);
+                answer = max(answer, x);
+            }
+        }
+    }
+    cout << answer << endl;
+    return 0;
 }
