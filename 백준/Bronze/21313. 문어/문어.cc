76965978fec38ddd9efaa345e278d7af0f006574
Hyeon Juun <81156795+HyeonJuun@users.noreply.github.com>
2024-06-21 21:57:14 +0900

    [Bronze II] Title: 문어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int n;
+ 
+int main()
+{
+    cin >> n;
+ 
+    int a = n % 2 ? n - 1 : n;
+    for (int i = 0; i < a; i++)
+    {
+        if (i % 2)
+        {
+            cout << "2 ";
+        }
+        else cout << "1 ";
+    }
+ 
+    if (n % 2)
+    {
+        cout << "3";
+    }
+ 
+    return 0;
+}
