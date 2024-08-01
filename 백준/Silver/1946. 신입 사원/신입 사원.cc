bac96286ad84f1d1efa9cee0a96364743e76ea33
Hyeon Juun <rnclf1005@naver.com>
2024-08-01 14:35:40 +0900

    [Silver I] Title: 신입 사원, Time: 532 ms, Memory: 4192 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <set>
 using namespace std;
 
-int T,N;
+#define MAX 1000 + 10
+#define INF 987654321
 
-int main() {
-    cin >> T;
+int t, n;
+int a, b;
 
-    while(T--) {
-        cin >> N;
-        
-        vector<pair<int,int>> grades;
-        
-        for(auto i=0;i<N;i++) {
-            int x,y;
-            cin >> x>>y;
-            grades.push_back({x,y});
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        vector<pair<int, int>> v;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a >> b;
+            v.push_back({a, b});
         }
+        sort(v.begin(), v.end());
 
-        sort(grades.begin(),grades.end());
-
-        int temp=0;
-        int result=1;
-
-        for(auto i=1;i<N;i++) {
-            if(grades[temp].second > grades[i].second) {
-                result++;
-                temp=i;
+        int tmp = 0;
+        int answer = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (v[i].second < v[tmp].second)
+            {
+                answer++;
+                tmp = i;
             }
         }
-        cout << result<<"\n";
+        cout << answer << endl;
     }
+
+    return 0;
 }
