98376230f3ab0b42a63b5a083fa6ba4574b08bc6
Hyeon Juun <rnclf1005@naver.com>
2024-08-29 18:37:03 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2344 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,95 +1,110 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 #include <queue>
-
+#include <cstring>
+#include <cmath>
+#include <set>
+#include <map>
 using namespace std;
 
-int n, m, k;
+#define MAX 1000 + 10
+#define INF 987654321
+
+int n, m, x;
 int a, b, c;
+vector<pair<int, int>> mp[MAX];
+vector<pair<int, int>> rev_mp[MAX];
+int dist[MAX];
+int answer[MAX];
 
-vector<pair<int, int>> v[1010];
-vector<pair<int, int>> revv[1010];
-int dist[1010];
-int ans[1010];
-void get_dist(int start)
+void resetDist()
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>,
-        greater<pair<int, int>>> pq;
-    for (int i = 1; i <= n; i++)
-        dist[i] = 1000000000;
+    for (int i = 1; i <= MAX; i++)
+        dist[i] = INF;
+}
+void getDist_rev(int start)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
+    pq.push({0, start});
     dist[start] = 0;
-    pq.push({ 0, start });
+
     while (!pq.empty())
     {
         int cur = pq.top().second;
-        int d = pq.top().first;
+        int cost = pq.top().first;
         pq.pop();
-        if (dist[cur] != d)
+
+        if (dist[cur] != cost)
             continue;
-
-        for (int i = 0; i < v[cur].size(); i++)
+        for (int i = 0; i < rev_mp[cur].size(); i++)
         {
-            int nxt = v[cur][i].first;
+            int nx = rev_mp[cur][i].first;
+            int nxCost = rev_mp[cur][i].second;
 
-            if (dist[nxt] > dist[cur] + v[cur][i].second)
+            if (dist[nx] > dist[cur] + nxCost)
             {
-                dist[nxt] = dist[cur] + v[cur][i].second;
-                pq.push({ dist[nxt], nxt });
+                dist[nx] = dist[cur] + nxCost;
+                pq.push({dist[nx], nx});
             }
         }
     }
 }
-void get_revdist(int start)
+void getDist(int start)
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>,
-        greater<pair<int, int>>> pq;
-    for (int i = 1; i <= n; i++)
-        dist[i] = 1000000000;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
+    pq.push({0, start});
     dist[start] = 0;
-    pq.push({ 0, start });
+
     while (!pq.empty())
     {
         int cur = pq.top().second;
-        int d = pq.top().first;
+        int cost = pq.top().first;
         pq.pop();
-        if (dist[cur] != d)
+
+        if (dist[cur] != cost)
             continue;
-
-        for (int i = 0; i < revv[cur].size(); i++)
+        for (int i = 0; i < mp[cur].size(); i++)
         {
-            int nxt = revv[cur][i].first;
+            int nx = mp[cur][i].first;
+            int nxCost = mp[cur][i].second;
 
-            if (dist[nxt] > dist[cur] + revv[cur][i].second)
+            if (dist[nx] > dist[cur] + nxCost)
             {
-                dist[nxt] = dist[cur] + revv[cur][i].second;
-                pq.push({ dist[nxt], nxt });
+                dist[nx] = dist[cur] + nxCost;
+                pq.push({dist[nx], nx});
             }
         }
     }
 }
-int main() {
-
-    cin >> n >> m >> k;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m >> x;
     for (int i = 0; i < m; i++)
     {
         cin >> a >> b >> c;
-        v[a].push_back({ b, c });
-        revv[b].push_back({ a, c });
+        mp[a].push_back({b, c});
+        rev_mp[b].push_back({a, c});
     }
-    get_dist(k);
-    int res = 0;
+
+    resetDist();
+    getDist_rev(x);
     for (int i = 1; i <= n; i++)
-        ans[i] = dist[i];
-    get_revdist(k);
+        answer[i] += dist[i];
+    resetDist();
+    getDist(x);
     for (int i = 1; i <= n; i++)
-        ans[i] += dist[i];
+        answer[i] += dist[i];
+
+    int ans = 0;
     for (int i = 1; i <= n; i++)
-    {
-        res = max(res, ans[i]);
-    }
-    cout << res;
+        ans = max(ans, answer[i]);
+    cout << ans << endl;
     return 0;
 }
