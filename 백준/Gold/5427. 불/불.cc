265f3cea830b7b7c8e53d6bb765b66f43fad5187
Hyeon Juun <rnclf1005@naver.com>
2024-09-02 16:04:22 +0900

    [Gold IV] Title: 불, Time: 52 ms, Memory: 13224 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,126 +1,124 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 #include <vector>
-
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <set>
+#include <map>
 using namespace std;
 
-const int MAX = 1001;
+#define MAX 1000 + 10
+#define INF 987654321
 
-int n, m, t;
-int answer;
-bool flag;
-string map[MAX];
+int tc, h, w, answer;
+
+string mp[MAX];
 int dist1[MAX][MAX];
 int dist2[MAX][MAX];
 
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, 1, 0, -1 };
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> tc;
 
-int main() {
+    while (tc--)
+    {
+        cin >> w >> h;
+        queue<pair<int, int>> fire;
+        queue<pair<int, int>> sang;
+        bool flag = false;
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	cin >> t;
-	while (t--)
-	{
-		flag = false;
-		queue<pair<int, int>> q1;
-		queue<pair<int, int>> q2;
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                dist1[i][j] = -1;
+                dist2[i][j] = -1;
+            }
+        }
+        for (int i = 0; i < h; i++)
+            cin >> mp[i];
 
-		cin >> m >> n;
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (mp[i][j] == '*')
+                {
+                    fire.push({i, j});
+                    dist1[i][j] = 0;
+                }
+                if (mp[i][j] == '@')
+                {
+                    sang.push({i, j});
+                    dist2[i][j] = 0;
+                }
+            }
+        }
 
+        while (!fire.empty())
+        {
+            int x = fire.front().first;
+            int y = fire.front().second;
+            fire.pop();
 
-		for (int i = 0; i < n; i++)
-			cin >> map[i];
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				dist1[i][j] = -1;
-				dist2[i][j] = -1;
-			}
-		}
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w)
+                    continue;
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (map[i][j] == '*')
-				{
-					q1.push({ i, j });
-					dist1[i][j] = 0;
-				}
-				if (map[i][j] == '@')
-				{
-					q2.push({ i, j });
-					dist2[i][j] = 0;
-				}
-			}
-		}
+                if (dist1[nx][ny] >= 0 || mp[nx][ny] == '#')
+                    continue;
 
-		while (!q1.empty())
-		{
-			int x = q1.front().first;
-			int y = q1.front().second;
-			q1.pop();
+                dist1[nx][ny] = dist1[x][y] + 1;
+                fire.push({nx, ny});
+            }
+        }
+        while (!sang.empty())
+        {
+            int x = sang.front().first;
+            int y = sang.front().second;
+            sang.pop();
 
-			for (int i = 0; i < 4; i++)
-			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-				if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-					continue;
-				if (dist1[nx][ny] >= 0 || map[nx][ny] == '#')
-					continue;
-				dist1[nx][ny] = dist1[x][y] + 1;
-				q1.push({ nx, ny });
-			}
-		}
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w)
+                {
+                    answer = dist2[x][y] + 1;
+                    flag = true;
+                    break;
+                }
+                if (dist2[nx][ny] >= 0 || mp[nx][ny] == '#')
+                    continue;
+                if (dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[x][y] + 1)
+                    continue;
 
-		while (!q2.empty())
-		{
-			int x = q2.front().first;
-			int y = q2.front().second;
-			q2.pop();
+                dist2[nx][ny] = dist2[x][y] + 1;
+                sang.push({nx, ny});
+            }
+            if (flag)
+                break;
+        }
+        if (flag)
+            cout << answer << endl;
+        else
+            cout << "IMPOSSIBLE" << endl;
 
-			for (int i = 0; i < 4; i++)
-			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-
-				if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-				{
-					answer = dist2[x][y] + 1;
-					flag = true;
-					break;
-				}
-				if (dist2[nx][ny] >= 0 || map[nx][ny] == '#')
-					continue;
-				if (dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[x][y] + 1)
-					continue;
-
-				dist2[nx][ny] = dist2[x][y] + 1;
-				q2.push({ nx, ny });
-			}
-			if (flag)
-				break;
-		}
-
-		if (flag)
-			cout << answer << endl;
-		else
-			cout << "IMPOSSIBLE" << endl;
-
-		for (int i = 0; i < n; i++)
-			map[i] = "";
-
-	}
-	
-	return 0;
+        for (int i = 0; i < h; i++)
+            mp[i] = "";
+    }
+    return 0;
 }
