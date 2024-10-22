6bcc95eb73f7bc75e47abcbda86ec1d2a70a494b
Hyeon Juun <rnclf1005@naver.com>
2024-10-22 17:38:04 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4608 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,53 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <cstring>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
 
+#define MAX 500 + 10
+#define INF 2e9
 using namespace std;
 
-#define MAX 500 + 1
-
-int n, m, answer;
-int map[MAX][MAX];
-int visited[MAX][MAX];
+int n, m;
+int mp[MAX][MAX];
+int dp[MAX][MAX];
 
 int dx[] = {0, 0, -1, 1};
 int dy[] = {1, -1, 0, 0};
 
 int solve(int x, int y)
 {
-    if (x == n - 1 && y == m - 1)
-        return 1;
-    if (visited[x][y] != -1)
-        return visited[x][y];
-
-    visited[x][y] = 0;
+    if (x == 0 && y == 0)
+        return dp[x][y] = 1;
+    if (dp[x][y] != INF)
+        return dp[x][y];
+    // cout << x << " " << y << endl;
+    int res = 0;
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dx[i];
         int ny = y + dy[i];
+        // cout << nx << " " << ny << endl;
         if (nx < 0 || ny < 0 || nx >= n || ny >= m)
             continue;
-
-        if (map[nx][ny] < map[x][y])
-        {
-            visited[x][y] += solve(nx, ny);
-        }
+        if (mp[x][y] >= mp[nx][ny])
+            continue;
+        res += solve(nx, ny);
     }
-    return visited[x][y];
+    return dp[x][y] = res;
 }
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            cin >> map[i][j];
-            visited[i][j] = -1;
+            cin >> mp[i][j];
+            dp[i][j] = INF;
         }
     }
-    cout << solve(0, 0) << endl;
+    int res = solve(n - 1, m - 1);
+    cout << res << endl;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //         cout << dp[i][j] << " ";
+    //     cout << endl;
+    // }
+    return 0;
 }
