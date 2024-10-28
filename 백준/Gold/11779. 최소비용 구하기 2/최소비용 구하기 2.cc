bf21791d886da6af4aaf2d03a67564d899b32ac2
Hyeon Juun <rnclf1005@naver.com>
2024-10-28 18:06:05 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -1,37 +1,46 @@
-#include<iostream>
-#include<algorithm>
-#include <queue>
+#include <iostream>
+#include <string>
+#include <algorithm>
 #include <vector>
+#include <cstring>
+#include <queue>
 
+#define MAX 1000 + 10
+#define INF 2e9
 using namespace std;
 
-const int INF = 2e8;
 int n, m;
 int a, b, c;
+int start_pos, target_pos;
+int dist[MAX];
+int route[MAX];
+vector<int> answer;
+vector<pair<int, int>> graph[MAX];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-vector<pair<int, int>> graph[1010];
-priority_queue<pair<int, int>, vector< pair<int, int>>, greater< pair<int, int>>> pq;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-int dist[1010];
-int route[1010];
-
-int main() {
-    
     cin >> n >> m;
-    
+
     for (int i = 0; i < m; i++)
     {
         cin >> a >> b >> c;
-        graph[a].push_back({ b, c });
+        graph[a].push_back({b, c});
     }
 
-    cin >> a >> b;
+    cin >> start_pos >> target_pos;
+
     for (int i = 1; i <= n; i++)
         dist[i] = INF;
 
-    pq.push({ 0, a });
-    dist[a] = 0;
-    route[a] = 0;
+    pq.push({0, start_pos});
+    dist[start_pos] = 0;
+    route[start_pos] = 0;
+
     while (!pq.empty())
     {
         int cur = pq.top().second;
@@ -43,30 +52,30 @@ int main() {
 
         for (int i = 0; i < graph[cur].size(); i++)
         {
-            int nxt = graph[cur][i].first;
+            int nx = graph[cur][i].first;
+            int ncost = graph[cur][i].second;
 
-            if (dist[nxt] > dist[cur] + graph[cur][i].second)
+            if (dist[nx] > dist[cur] + ncost)
             {
-                dist[nxt] = dist[cur] + graph[cur][i].second;
-                route[nxt] = cur;
-                pq.push({ dist[nxt], nxt });
-
+                dist[nx] = dist[cur] + ncost;
+                route[nx] = cur;
+                pq.push({dist[nx], nx});
             }
         }
     }
-    vector<int> answer;
-    int x = b;
-  
+    int x = target_pos;
     while (x)
     {
         answer.push_back(x);
         x = route[x];
     }
     reverse(answer.begin(), answer.end());
-
-    cout << dist[b] << endl;
-    cout << answer.size() << endl;
+    cout << dist[target_pos] << endl
+         << answer.size() << endl;
     for (int i = 0; i < answer.size(); i++)
+    {
         cout << answer[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
