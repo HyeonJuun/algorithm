4a956c0af911dab4ecb5d3902bb8284a7ca00efb
Hyeon Juun <rnclf1005@naver.com>
2024-10-30 17:54:58 +0900

    [Gold III] Title: 불!, Time: 40 ms, Memory: 11080 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,115 +1,107 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
-#include <queue>
 #include <vector>
+#include <cstring>
+#include <queue>
+#include <cmath>
 
+#define MAX 1000 + 10
+#define INF 2e9
 using namespace std;
 
-const int MAX = 1001;
+int r, c, answer = INF;
+string mp[MAX];
+int dist_j[MAX][MAX];
+int dist_f[MAX][MAX];
 
-int n, m;
-int answer;
-string map[MAX];
-bool flag;
-int dist1[MAX][MAX];
-int dist2[MAX][MAX];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, 1, 0, -1 };
+queue<pair<int, int>> q_f;
+bool flag = false;
+queue<pair<int, int>> q_j;
+void bfs()
+{
+    while (!q_f.empty())
+    {
+        int curx = q_f.front().first;
+        int cury = q_f.front().second;
+        q_f.pop();
 
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
 
-int main() {
+            if (nx < 0 || ny < 0 || nx >= r || ny >= c || mp[nx][ny] == '#' || dist_f[nx][ny] >= 0)
+                continue;
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	queue<pair<int, int>> q1;
-	queue<pair<int, int>> q2;
+            dist_f[nx][ny] = dist_f[curx][cury] + 1;
+            q_f.push({nx, ny});
+        }
+    }
 
-	cin >> n >> m;
+    while (!q_j.empty())
+    {
+        int curx = q_j.front().first;
+        int cury = q_j.front().second;
+        q_j.pop();
 
-	for (int i = 0; i < n; i++)
-		cin >> map[i];
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			dist1[i][j] = -1;
-			dist2[i][j] = -1;
-		}
-	}
+            if (nx < 0 || ny < 0 || nx >= r || ny >= c)
+            {
+                answer = dist_j[curx][cury] + 1;
+                flag = true;
+                break;
+            }
+            if (mp[nx][ny] == '#' || dist_j[nx][ny] >= 0)
+                continue;
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (map[i][j] == 'F')
-			{
-				q1.push({ i, j });
-				dist1[i][j] = 0;
-			}
-			if (map[i][j] == 'J')
-			{
-				q2.push({ i, j });
-				dist2[i][j] = 0;
-			}
-		}
-	}
+            if (dist_j[curx][cury] + 1 >= dist_f[nx][ny] && dist_f[nx][ny] != -1)
+                continue;
+            q_j.push({nx, ny});
+            dist_j[nx][ny] = dist_j[curx][cury] + 1;
+        }
+        if (flag)
+            break;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	while (!q1.empty())
-	{
-		int x = q1.front().first;
-		int y = q1.front().second;
-		q1.pop();
+    cin >> r >> c;
 
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-				continue;
-			if (dist1[nx][ny] >= 0 || map[nx][ny] == '#')
-				continue;
-			dist1[nx][ny] = dist1[x][y] + 1;
-			q1.push({ nx, ny });
-		}
-	}
-
-	while (!q2.empty())
-	{
-		int x = q2.front().first;
-		int y = q2.front().second;
-		q2.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-			{
-				answer = dist2[x][y] + 1;
-				flag = true;
-				break;
-			}
-			if (dist2[nx][ny] >= 0 || map[nx][ny] == '#')
-				continue;
-			if (dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[x][y] + 1)
-				continue;
-
-			dist2[nx][ny] = dist2[x][y] + 1;
-			q2.push({ nx, ny });
-		}
-		if (flag)
-			break;
-	}
-
-	if (flag)
-		cout << answer;
-	else
-		cout << "IMPOSSIBLE";
-	return 0;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> mp[i];
+        for (int j = 0; j < c; j++)
+        {
+            dist_f[i][j] = -1;
+            dist_j[i][j] = -1;
+            if (mp[i][j] == 'J')
+            {
+                q_j.push({i, j});
+                dist_j[i][j] = 0;
+            }
+            if (mp[i][j] == 'F')
+            {
+                q_f.push({i, j});
+                dist_f[i][j] = 0;
+            }
+        }
+    }
+    bfs();
+    if (!flag)
+        cout << "IMPOSSIBLE" << endl;
+    else
+        cout << answer << endl;
+    return 0;
 }
